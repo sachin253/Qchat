9aaeaa0e40121e908fd8f6bd9eb9880c048dace4
sachin253 <sachinsammy1212@gmail.com>
2020-07-21 20:05:43 +0530

    Add splash screen

---
+++ app/src/main/java/com/example/qchat/splashActivity.java
@@ -0,0 +1,17 @@
+package com.example.qchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+}
